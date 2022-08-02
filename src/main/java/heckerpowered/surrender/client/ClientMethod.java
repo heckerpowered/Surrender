78305abded8b660fa3d2a18f4066570694d55827
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2022-08-02 18:31:56 +0800

    Remove unnecessary code

--- src/main/java/heckerpowered/surrender/client/ClientMethod.java
+++ src/main/java/heckerpowered/surrender/client/ClientMethod.java
@@ -1,10 +1,7 @@
 package heckerpowered.surrender.client;
 
-import javax.annotation.Nonnull;
-
 import net.minecraft.client.Minecraft;
 import net.minecraft.sounds.SoundEvents;
-import net.minecraft.world.item.ItemStack;
 
 public final class ClientMethod {
     private ClientMethod() {
@@ -20,10 +17,5 @@ public final class ClientMethod {
         minecraft.submit(() -> {
             minecraft.player.playSound(SoundEvents.PLAYER_ATTACK_SWEEP, 1, 1);
         });
-    }
-
-    public static final void displayStack(@Nonnull final ItemStack stack) {
-        final var minecraft = Minecraft.getInstance();
-        minecraft.gameRenderer.displayItemActivation(stack);
     }
 }
